acbd528a271df6cdc3b3d09385ead475c4628ef7
nickc-ci <nchamberlain@cambridge-intl.com>
2015-12-01 17:07:12 -0500

    Trying to simulate expensive calculation

--- LinqSubexpressions/DoubleIteration.cs
+++ LinqSubexpressions/DoubleIteration.cs
@@ -1,10 +1,34 @@
-﻿using Xunit;
+﻿using System.Linq;
+using System.Threading;
+using LinqSubexpressions.DAL;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace LinqSubexpressions {
     public class DoubleIteration {
+        private readonly ITestOutputHelper _output;
+
+        public DoubleIteration(ITestOutputHelper output) {
+            _output = output;
+        }
         [Fact]
         public void Iterate_results_twice() {
-            
-        }   
+            using (var db = new MyDbContext()) {
+                var results = db.SalesOrderDetails
+                    .Select(sod => new {
+                        sod.OrderQty,
+                        sod.UnitPrice,
+                        SlowStatisticCalculation = sod.Sleep100()
+                    }).ToList();
+
+                _output.WriteLine(results.ToJson());
+            }
+        }
+    }
+    public static class ExpensiveComputations {
+        public static SalesOrderDetail Sleep100(this SalesOrderDetail query) {
+            Thread.Sleep(100);
+            return query;
+        }
     }
 }
---
+++ LinqSubexpressions/JsonExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace LinqSubexpressions {
+    public static class JsonExtensions {
+        public static string ToJson(this object obj) {
+            return JsonConvert.SerializeObject(obj, Formatting.Indented, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+        }
+    }
+}
