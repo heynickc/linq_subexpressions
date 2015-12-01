6d9f08b21669c463673a5f31f984a63bb86289e1
nickc-ci <nchamberlain@cambridge-intl.com>
2015-12-01 15:42:19 -0500

    Project for messing around with Linq.Subexpressions

---
+++ Linq.Subexpressions/DoubleInteration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Linq.Subexpressions {
+    public class DoubleIteration {
+        [Fact]
+        public void Iterate_results_twice() {
+            
+        }   
+    }
+}
