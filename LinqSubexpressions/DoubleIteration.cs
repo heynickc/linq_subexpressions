216c0299310e5319a5d2c54fff645126b7681104
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2015-12-01 21:46:43 -0500

    Working on sample code

--- LinqSubexpressions/DoubleIteration.cs
+++ LinqSubexpressions/DoubleIteration.cs
@@ -18,17 +18,14 @@ namespace LinqSubexpressions {
                     .Select(sod => new {
                         sod.OrderQty,
                         sod.UnitPrice,
-                        SlowStatisticCalculation = sod.Sleep100()
+                        ExtendedPrice = sod.OrderQty*sod.UnitPrice
                     }).ToList();
 
                 _output.WriteLine(results.ToJson());
             }
         }
-    }
-    public static class ExpensiveComputations {
-        public static SalesOrderDetail Sleep100(this SalesOrderDetail query) {
-            Thread.Sleep(100);
-            return query;
+        public decimal GetExtendedPrice(short qty, decimal unitPrice) {
+            return qty * unitPrice;
         }
     }
 }
