b067fdf1218e87f6fb0701e70c8a2716f8d330a0
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2015-12-02 21:36:08 -0500

    Variations

--- LinqSubexpressions/DoubleIteration.cs
+++ LinqSubexpressions/DoubleIteration.cs
@@ -10,14 +10,15 @@ using Xunit;
 using Xunit.Abstractions;
 
 namespace LinqSubexpressions {
-    public class DoubleIteration {
+    public class DoubleIteration : IDisposable {
         private readonly ITestOutputHelper _output;
-
+        private readonly IDisposable _logCapture;
         public DoubleIteration(ITestOutputHelper output) {
             _output = output;
+            _logCapture = LoggingHelper.Capture(_output);
         }
         [Fact]
-        public void Iterate_results_twice_lineTotal() {
+        public void Multliple_method_calls_linetotal() {
             using (var db = new FakeMyDbContext()) {
 
                 var sods = new List<SalesOrderDetail>();
@@ -31,12 +32,7 @@ namespace LinqSubexpressions {
                 }
                 db.SalesOrderDetails.AddRange(sods);
 
-                var logger = new LoggerConfiguration()
-                    .MinimumLevel.Debug()
-                    .WriteTo.XunitTestOutput(_output)
-                    .CreateLogger();
-
-                using (logger.BeginTimedOperation("Iterate twice timer")) {
+                using (Log.Logger.BeginTimedOperation("Calculating quick total")) {
                     var results = db.SalesOrderDetails
                         .Where(sod => sod.SalesOrderId == 71774)
                         .ToList()
@@ -45,6 +41,50 @@ namespace LinqSubexpressions {
                             sod.UnitPrice,
                             LineTotal = CalculateLinePrice(sod.OrderQty, sod.UnitPrice)
                         }).ToList();
+                    //_output.WriteLine(results.ToJson());
+                }
+            }
+        }
+        [Fact]
+        public void Multliple_method_calls_margin() {
+            using (var db = new FakeMyDbContext()) {
+
+                var sods = new List<SalesOrderDetail>();
+                for (int i = 0; i < 10; i++) {
+                    sods.Add(new SalesOrderDetail() {
+                        SalesOrderId = 71774,
+                        ProductId = 905,
+                        OrderQty = 4,
+                        UnitPrice = 218.454m
+                    });
+                }
+                db.SalesOrderDetails.AddRange(sods);
+
+                var products = new Product() {
+                    ProductId = 905,
+                    StandardCost = 199.3757m
+                };
+                db.Products.Add(products);
+
+                using (Log.Logger.BeginTimedOperation("Calculating margin slowly")) {
+                    var results = db.SalesOrderDetails
+                        .Where(sod => sod.SalesOrderId == 71774)
+                        .Join(db.Products,
+                            sod => sod.ProductId,
+                            product => product.ProductId,
+                            (sod, product) => new { sod, product })
+                        .ToList()
+                        .Select(li => new {
+                            li.sod.OrderQty,
+                            li.sod.UnitPrice,
+                            li.product.StandardCost,
+                            LineTotal = CalculateLinePrice(li.sod.OrderQty, li.sod.UnitPrice),
+                            LineCost = CalculateLineCost(li.sod.OrderQty, li.product.StandardCost),
+                            Margin = CalculateMargin(
+                                CalculateLineCost(li.sod.OrderQty, li.product.StandardCost),
+                                CalculateLinePrice(li.sod.OrderQty, li.sod.UnitPrice))
+                        }).ToList();
+
                     //_output.WriteLine(results.ToJson());
                 }
             }
@@ -70,47 +110,151 @@ namespace LinqSubexpressions {
                 };
                 db.Products.Add(products);
 
-                var logger = new LoggerConfiguration()
-                    .MinimumLevel.Debug()
-                    .WriteTo.XunitTestOutput(_output)
-                    .CreateLogger();
-
-                using (logger.BeginTimedOperation("Iterate twice timer")) {
+                using (Log.Logger.BeginTimedOperation("Calculating margin with second iteration")) {
                     var results = db.SalesOrderDetails
                         .Where(sod => sod.SalesOrderId == 71774)
                         .Join(db.Products,
                             sod => sod.ProductId,
                             product => product.ProductId,
-                            (sod, product) => new {sod, product})
+                            (sod, product) => new { sod, product })
                         .ToList()
                         .Select(li => new {
                             li.sod.OrderQty,
                             li.sod.UnitPrice,
                             li.product.StandardCost,
                             LineTotal = CalculateLinePrice(li.sod.OrderQty, li.sod.UnitPrice),
-                            LineCost = CalculateLineCost(li.sod.OrderQty, li.product.StandardCost),
-                            Margin = CalculateMargin(
-                                CalculateLineCost(li.sod.OrderQty, li.product.StandardCost), 
-                                CalculateLinePrice(li.sod.OrderQty, li.sod.UnitPrice))
+                            LineCost = CalculateLineCost(li.sod.OrderQty, li.product.StandardCost)
                         }).ToList();
-                    
-                    _output.WriteLine(results.ToJson());
+
+                    var resultsAgain = results
+                        .Select(li => new {
+                            li.OrderQty,
+                            li.UnitPrice,
+                            li.StandardCost,
+                            li.LineTotal,
+                            li.LineCost,
+                            Margin = CalculateMargin(li.LineCost, li.LineTotal)
+                        }).ToList();
+
+                    //_output.WriteLine(resultsAgain.ToJson());
+                }
+            }
+        }
+
+        [Fact]
+        public void Multiline_lambda_statement() {
+            using (var db = new FakeMyDbContext()) {
+
+                var sods = new List<SalesOrderDetail>();
+                for (int i = 0; i < 10; i++) {
+                    sods.Add(new SalesOrderDetail() {
+                        SalesOrderId = 71774,
+                        ProductId = 905,
+                        OrderQty = 4,
+                        UnitPrice = 218.454m
+                    });
+                }
+                db.SalesOrderDetails.AddRange(sods);
+
+                var products = new Product() {
+                    ProductId = 905,
+                    StandardCost = 199.3757m
+                };
+                db.Products.Add(products);
+
+                using (Log.Logger.BeginTimedOperation("Calculating margin with second iteration")) {
+                    var results = db.SalesOrderDetails
+                        .Where(sod => sod.SalesOrderId == 71774)
+                        .Join(db.Products,
+                            sod => sod.ProductId,
+                            product => product.ProductId,
+                            (sod, product) => new { sod, product })
+                        .ToList()
+                        .Select(li => {
+                            var lineTotal = CalculateLinePrice(li.sod.OrderQty, li.sod.UnitPrice);
+                            var lineCost = CalculateLineCost(li.sod.OrderQty, li.product.StandardCost);
+                            return new {
+                                li.sod.OrderQty,
+                                li.sod.UnitPrice,
+                                li.product.StandardCost,
+                                LineTotal = lineTotal,
+                                LineCost = lineCost,
+                                Margin = CalculateMargin(lineCost, lineTotal)
+                            };
+                        }).ToList();
+
+                    //_output.WriteLine(resultsAgain.ToJson());
+                }
+            }
+        }
+
+        [Fact]
+        public void Linq_subexpression() {
+            using (var db = new FakeMyDbContext()) {
+
+                var sods = new List<SalesOrderDetail>();
+                for (int i = 0; i < 10; i++) {
+                    sods.Add(new SalesOrderDetail() {
+                        SalesOrderId = 71774,
+                        ProductId = 905,
+                        OrderQty = 4,
+                        UnitPrice = 218.454m
+                    });
+                }
+                db.SalesOrderDetails.AddRange(sods);
+
+                var products = new Product() {
+                    ProductId = 905,
+                    StandardCost = 199.3757m
+                };
+                db.Products.Add(products);
+
+                using (Log.Logger.BeginTimedOperation("Calculating margin with second iteration")) {
+                    var results = db.SalesOrderDetails
+                        .Where(sod => sod.SalesOrderId == 71774)
+                        .Join(db.Products,
+                            sod => sod.ProductId,
+                            product => product.ProductId,
+                            (sod, product) => new { sod, product })
+                        .ToList()
+                        .Select(li =>
+                            new {
+                                lineTotal = CalculateLinePrice(li.sod.OrderQty, li.sod.UnitPrice),
+                                lineCost = CalculateLineCost(li.sod.OrderQty, li.product.StandardCost),
+                                li
+                            })
+                        .Select(lineItem => {
+                            return new {
+                                lineItem.li.sod.OrderQty,
+                                lineItem.li.sod.UnitPrice,
+                                lineItem.li.product.StandardCost,
+                                LineTotal = lineItem.lineTotal,
+                                LineCost = lineItem.lineCost,
+                                Margin = CalculateMargin(lineItem.lineCost, lineItem.lineTotal)
+                            };
+                        }).ToList();
+
+                    //_output.WriteLine(resultsAgain.ToJson());
                 }
             }
         }
 
         public decimal CalculateLinePrice(short qty, decimal unitPrice) {
-            //Thread.Sleep(1000);
+            Thread.Sleep(100);
             return qty * unitPrice;
         }
 
         public decimal CalculateLineCost(short qty, decimal unitCost) {
-            //Thread.Sleep(10000);
+            Thread.Sleep(200);
             return qty * unitCost;
         }
         public decimal CalculateMargin(decimal lineCost, decimal linePrice) {
-            var margin = (linePrice - lineCost)/linePrice;
+            var margin = (linePrice - lineCost) / linePrice;
             return margin;
+        }
+
+        public void Dispose() {
+            _logCapture.Dispose();
         }
     }
 }
---
+++ LinqSubexpressions/LoggingHelpers.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using Serilog;
+using Serilog.Context;
+using Serilog.Events;
+using Serilog.Formatting.Display;
+using Xunit.Abstractions;
+
+namespace LinqSubexpressions {
+    // http://dhickey.ie/2015/06/capturing-log-output-in-tests-with-xunit2/
+    // https://github.com/damianh/CapturingLogOutputWithXunit2AndParallelTests/blob/master/src/Lib.Tests/LoggingHelper.cs
+    internal static class LoggingHelper {
+        private static readonly Subject<LogEvent> s_logEventSubject = new Subject<LogEvent>();
+        private const string CaptureCorrelationIdKey = "CaptureCorrelationId";
+        private static readonly MessageTemplateTextFormatter s_formatter = new MessageTemplateTextFormatter(
+                "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}", null);
+
+        static LoggingHelper() {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo
+                .Observers(observable => observable.Subscribe(logEvent => s_logEventSubject.OnNext(logEvent)))
+                .Enrich.FromLogContext()
+                .CreateLogger();
+        }
+
+        public static IDisposable Capture(ITestOutputHelper testOutputHelper) {
+            var captureId = Guid.NewGuid();
+
+            Func<LogEvent, bool> filter = logEvent =>
+                logEvent.Properties.ContainsKey(CaptureCorrelationIdKey) &&
+                logEvent.Properties[CaptureCorrelationIdKey].ToString() == captureId.ToString();
+
+            var subscription = s_logEventSubject.Where(filter).Subscribe(logEvent => {
+                using (var writer = new StringWriter()) {
+                    s_formatter.Format(logEvent, writer);
+                    testOutputHelper.WriteLine(writer.ToString());
+                }
+            });
+            var pushProperty = LogContext.PushProperty(CaptureCorrelationIdKey, captureId);
+
+            return new DisposableAction(() => {
+                subscription.Dispose();
+                pushProperty.Dispose();
+            });
+        }
+
+        private class DisposableAction : IDisposable {
+            private readonly Action _action;
+
+            public DisposableAction(Action action) {
+                _action = action;
+            }
+
+            public void Dispose() {
+                _action();
+            }
+        }
+    }
+}
